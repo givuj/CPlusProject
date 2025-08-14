28afd08fbdd2ba1d08be1bec93ebefb594bcedf7
givuj <2920152382@qq.com>
2025-08-14 14:15:04 +0800

    1

---
+++ Project1/Project1/Project1.cpp
@@ -0,0 +1,47 @@
+﻿#include <mutex>
+
+//反转链表
+//struct ListNode
+//{
+//	int val;
+//	ListNode* next;
+//	ListNode(int x)
+//	{
+//		val = x;
+//		next = nullptr;
+//	}
+//};
+//ListNode* reverseList(ListNode* head) {
+//	ListNode* pre = nullptr;
+//	ListNode* current = head;
+//	while (current)
+//	{
+//		ListNode* next = current->next;
+//		current->next = pre;
+//		pre = current;
+//		current = next;
+//	}
+//	return pre;
+//}
+
+
+//确保线程安全的单例模式
+class Singleton
+{
+public:
+	Singleton() {};
+	static Singleton* instance;
+	static std::mutex mtx;
+
+private:
+	static Singleton* getInstance()
+	{
+		std::lock_guard<std::mutex> lock(mtx);
+		if (instance == nullptr)
+		{
+			instance = new Singleton();
+		}
+		return instance;
+	}
+};
+
